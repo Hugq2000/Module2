8ad0e5e5f98dc1ba6b20a6c3e9a4fca70285be96
= <=>
2021-04-12 17:36:26 +0700

    Module2 second time

---
+++ src/Module2/Bai3.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Module2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * psvm +tab pa+crtl+space : ep kieu asdasff
+ *
+ * @author Hung
+ */
+public class Bai3 implements Bai3_Interface {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Nhập bán kính: ");
+        double r = Double.parseDouble(input.readLine());
+        Bai3_Interface circle = (Double x1, Double x2) -> x1 * Math.pow(x2, 2);
+        System.out.println("S = PI * (r*r) = " + circle.calArea(Math.PI, r));
+        System.out.println("Nhập chiều dài: ");
+        double H = Double.parseDouble(input.readLine());
+        System.out.println("Nhập chiều rộng: ");
+        double W = Double.parseDouble(input.readLine());
+        Bai3_Interface rectangle = (Double x1, Double x2) -> x1 * x2;
+        System.out.println("S = W x H = " + rectangle.calArea(W, H));
+    }
+
+    @Override
+    public double calArea(Double x1, Double x2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
